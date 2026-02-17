877ae5d6ce5123dcce0db82a1ffa145a959073d9
John Fox <foxj77@gmail.com>
2026-02-17 21:36:06 +0000

    feat(terraform): add VM admin username and SSH key variables
    Add type constraints to existing location and customer variables.
    Introduce new variables for web and backend VM admin usernames with
    default values, and SSH public key for backend authentication.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,26 @@
 variable "location" {
   description = "azure location to deploy resources"
+  type        = string
 }
 
 variable "customer" {
   description = "Customer name, used for naming of resources"
+  type        = string
+}
+
+variable "web_admin_username" {
+  description = "Admin username for the web VMSS"
+  type        = string
+  default     = "snapvideoadmin"
+}
+
+variable "backend_admin_username" {
+  description = "Admin username for the backend Linux VM"
+  type        = string
+  default     = "snapvideoadmin"
+}
+
+variable "backend_ssh_public_key" {
+  description = "SSH public key for the backend Linux VM admin user"
+  type        = string
 }
