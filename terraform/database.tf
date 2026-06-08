81f9ceb0708cc3a3ba1bdcebdf1ab6e3d19de1c0
John Fox <foxj77@gmail.com>
2026-06-08 14:52:15 +0100

    refactor(terraform): downgrade mysql sku and disable high availability
    Update the MySQL flexible server SKU from GP_Standard_D2ds_v4 to B_Standard_B1ms and change the availability zone to 3. Comment out the high_availability block to reduce costs and complexity.

--- terraform/database.tf
+++ terraform/database.tf
@@ -7,14 +7,13 @@ resource "azurerm_mysql_flexible_server" "snapvideo" {
   backup_retention_days  = 7
   delegated_subnet_id    = azurerm_subnet.database.id
   private_dns_zone_id    = azurerm_private_dns_zone.snapvideo.id
-  sku_name               = "GP_Standard_D2ds_v4"
-  zone                   = 1
+  sku_name               = "B_Standard_B1ms"
+  zone                   = "3"
   version                = "8.0.21"
 
-  high_availability {
-    mode                      = "ZoneRedundant"
-    standby_availability_zone = 2
-  }
+  # high_availability {
+  #   mode                      = "SameZone"
+  # }
 
   depends_on = [azurerm_private_dns_zone_virtual_network_link.snapvideo, azurerm_subnet.database, azurerm_key_vault.kv]
 
